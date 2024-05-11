7be163af0d3c4cd8cd8864133108b712ad8c469a
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2024-05-11 15:18:53 +0700

    feat: create a chat list layout and add several components
    compile a chat list box and add several components such as floating buttons and images

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -8,7 +8,7 @@ export default function PrimaryButton({
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2.5 bg-primary border border-transparent rounded-md text-sm font-semibold text-white dark:text-dark uppercase tracking-widest hover:bg-primary focus:bg-primary active:bg-primary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
+                `inline-flex items-center px-4 py-2.5 bg-primary border border-transparent rounded-md text-sm font-semibold text-white dark:text-dark uppercase tracking-widest hover:bg-primary focus:bg-primary active:bg-primary focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
                     disabled && "opacity-25"
                 } ` + className
             }
--- resources/js/Pages/Chat/ChatList.jsx
+++ resources/js/Pages/Chat/ChatList.jsx
@@ -1,10 +1,21 @@
+import ChatBox from "@/Components/ChatBox";
+import FloatingButton from "@/Components/FloatingButton";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Head } from "@inertiajs/react";
 
-export default function ChatList({ auth }) {
+export default function ChatList({ auth, messageList }) {
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="ChatList" />
+            <div id="chat-box" className="mt-2 flex flex-col">
+                {messageList.map((message) => (
+                    <ChatBox {...message} />
+                ))}
+            </div>
+            <FloatingButton className="h-14 w-14">
+                <FontAwesomeIcon icon={["fas", "comments"]} size="xl" />
+            </FloatingButton>
         </AuthenticatedLayout>
     );
 }
